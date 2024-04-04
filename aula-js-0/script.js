1bec53195b0810fd11436cd3b16aa3d2c8471a74
Arthur Pagani <112992377+acpagani@users.noreply.github.com>
2024-04-03 21:30:07 -0300

    aula0
    Introdução ao JavaScript

---
+++ aula-js-0/script.js
@@ -0,0 +1,9 @@
+/* alert("Olá Mundo!")
+confirm("Você deseja Sair?")
+*/
+
+var nome = prompt("Qual o seu nome?")
+var idade = prompt("Quantos anos você tem?")
+var profissao = prompt("Você trabalha com o que?")
+
+console.log(`Olá ${nome}, você tem ${idade} e trabalha como ${profissao}`)
