dc85d2c429a9fd558dafc212afeead205a711b7a
Arthur Pagani <112992377+acpagani@users.noreply.github.com>
2024-09-05 14:33:31 -0300

    aula11

---
+++ aula11/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <h1>Titulo da pagina</h1>
+    </>
+  )
+}
+
+export default App
---
+++ aula11/vite-project/src/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <h1 className="text-primaryFiap rounded text-9xl ml-6 font-bold flex justify-center md:text-green-700 bg-gray-900">Header</h1>
+    )
+}
